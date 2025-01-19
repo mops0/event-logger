27c469c7a2cc96e56d02669702554babb0d40131
szymczys <tomasz.k.szymczyszyn@nokia.com>
2025-01-19 14:00:38 +0100

    Springboot framework out of the box boilerplate for Write Service

---
+++ write-service/src/main/java/com/szymczys/writeservice/WriteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.szymczys.writeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WriteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WriteServiceApplication.class, args);
+	}
+
+}
---
+++ write-service/src/test/java/com/szymczys/writeservice/WriteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szymczys.writeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WriteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
